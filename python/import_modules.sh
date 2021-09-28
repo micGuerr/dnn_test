773ca62851d708c48cbec5a36148de63bf3bbfe0
micGuerr <52530590+micGuerr@users.noreply.github.com>
2021-09-28 14:51:21 +0200

    Update import_modules.sh

--- python/import_modules.sh
+++ python/import_modules.sh
@@ -3,6 +3,6 @@
 ! pip install tensorflow==2.6.0
 ! pip install sklearn==0.0
 ! pip install numpy==1.19.5
-! pip uninstall imgaug
+# ! pip uninstall imgaug
 # ! pip install imgaug==0.2.5
 # ! pip install matplotlib==3.4.3
